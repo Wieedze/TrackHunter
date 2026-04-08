ca1b7b01f374055b371aa3ec405b90c2d74ceb96
Maxime Saint-joannis <maxime.moodz@gmail.com>
2026-04-08 10:55:34 +0200

    Fix export statement for App component

--- src/App.tsx
+++ src/App.tsx
@@ -45,49 +45,4 @@ function App() {
   );
 }
 
-export default App;import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { Layout } from './components/layout/Layout.tsx';
-import { Home } from './pages/Home.tsx';
-import { Results } from './pages/Results.tsx';
-import { Wishlist } from './pages/Wishlist.tsx';
-import { Settings } from './pages/Settings.tsx';
-import { About } from './pages/About.tsx';
-import { Guide } from './pages/Guide.tsx';
-import { Blog } from './pages/Blog.tsx';
-import { ArticleLayout } from './pages/articles/ArticleLayout.tsx';
-import { Stats } from './pages/Stats.tsx';
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 24 * 60 * 60 * 1000, // 24h cache for search results
-      retry: 2,
-      refetchOnWindowFocus: false,
-    },
-  },
-});
-
-function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<Layout />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/results" element={<Results />} />
-            <Route path="/wishlist" element={<Wishlist />} />
-            <Route path="/settings" element={<Settings />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/guide" element={<Guide />} />
-            <Route path="/blog" element={<Blog />} />
-            <Route path="/blog/:slug" element={<ArticleLayout />} />
-            <Route path="/stats" element={<Stats />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </QueryClientProvider>
-  );
-}
-
 export default App;
